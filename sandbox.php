543acd721f3c4868471c19fa559c371f3ea6588b
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-07-30 19:13:26 -0500

    se añadio la session

---
+++ sandbox.php
@@ -0,0 +1,28 @@
+<?php
+
+// sessions
+if (isset($_POST['submit'])) {
+
+    session_start();
+
+    $_SESSION['name'] = $_POST['name'];
+
+    header('Location: index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>php tuts</title>
+</head>
+
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+        <input type="text" name="name">
+        <input type="submit" name="submit" value="submit">
+    </form>
+</body>
+
+</html>
